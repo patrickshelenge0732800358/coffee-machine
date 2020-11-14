34d988a98ec7bef93adf3c00466cac1a84ff0f3e
SHELENGE <patrickshelenge@gmail.com>
2020-11-14 16:06:08 +0200

    done with stage1

--- stage1.py
+++ stage1.py
@@ -1,2 +1,10 @@
 # commit to this work
 # i will change this works
+number_of_cup=input("how many number of cups of coffee?")
+total_amount_water=int(number_of_cup)*200
+total_amount_milk=int(number_of_cup)*50
+total_amount_coffee=int(number_of_cup)*15
+print("for" +str(number_of_cup)+ " you will need:")
+print(str(total_amount_water)+ "ml of water")
+print(str(total_amount_milk)+ "ml of milk")
+print(str(total_amount_coffee) +" g of coffee beans"
